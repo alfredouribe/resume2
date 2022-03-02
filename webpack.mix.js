8d8e7b85675d96890f65c26fcab245a3521d000b
Francisco Alfredo Uribe Sánchez <alfredo.uribe.sanchez@gmail.com>
2022-03-01 20:45:48 -0600

    add: juego batalla

--- webpack.mix.js
+++ webpack.mix.js
@@ -24,4 +24,5 @@ mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/init.js', 'public/js/init.js')
     .js('resources/js/Overworld.js', 'public/js/Overworld.js')
     .copy('resources/images/maps/', 'public/images/maps/')
-    .copy('resources/images/heroes/', 'public/images/heroes/');
+    .copy('resources/images/heroes/', 'public/images/heroes/')
+    .copy('resources/images/heroes/hero_mini/', 'public/images/heroes/hero_mini/');
